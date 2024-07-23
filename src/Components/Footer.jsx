c8bb3eb829ecf4b9c29048877141ebca929f1079
vinodkeshariya <keshariyavinod793@gmail.com>
2024-07-23 20:09:06 +0530

    add tailwind css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,43 +4,43 @@ import logo from "../img/krushna53Logo.png";
 function Footer(params) {
   return (
     <>
-      <div className="footer-section">
-        <div className="footerwrapper">
+      <div className="tw-bg-custom-gray">
+        <div className=" tw-flex tw-justify-between tw-w-[95%] tw-mx-auto">
           <div className="logo">
-            <img src={logo} alt="..." />
+            <img src={logo} alt="..." className="tw-w-[100px]"/>
           </div>
-          <p>Copyright All Rights Reserved</p>
-          <div className="footer-icons">
+          <p className="tw-flex tw-items-center tw-text-lg tw-text-center">Copyright All Rights Reserved</p>
+          <div className="tw-flex tw-max-w-full">
             <a
               href="https://bit.ly/3KhlwNE"
               target="_blank"
               rel="noreferrer"
-              className="footer-icon"
+              className="tw-m-auto"
             >
-              <i className="ri-github-fill icon"></i>
+              <i className="ri-github-fill tw-text-blue-700 tw-text-[27px] tw-inline-block tw-mx-1.5 hover:tw-text-customBrown"></i>
             </a>
             <a
               href="https://bit.ly/3WbQEoH"
               target="_blank"
               rel="noreferrer"
-              className="footer-icon"
+              className="tw-m-auto"
             >
-              <i className="ri-whatsapp-line icon"></i>
+              <i className="ri-whatsapp-line tw-text-blue-700 tw-text-[27px] tw-inline-block tw-mx-1.5 hover:tw-text-customBrown"></i>
             </a>
             <a
               href="mailto:info@krushna53.com"
-              className="footer-icon"
+              className="tw-m-auto"
             >
-              <i className="fas fa-envelope icon"></i>
+              <i className="fas fa-envelope   tw-text-blue-700 tw-text-[27px] tw-inline-block tw-mx-1.5 hover:tw-text-customBrown"></i>
             </a>
             {/* <a href="#" className="footer-icon"><i className="ri-instagram-line icon"></i></a> */}
             <a
               href="https://www.linkedin.com/company/krushna53"
               target="_blank"
               rel="noreferrer"
-              className="footer-icon"
+              className="footer-icon tw-m-auto"
             >
-              <i className="ri-linkedin-box-fill icon"></i>
+              <i className="ri-linkedin-box-fill  tw-text-blue-700 tw-text-[27px] tw-inline-block tw-mx-1.5 hover:tw-text-customBrown"></i>
             </a>
           </div>
         </div>
